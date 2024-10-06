7db9c58cfb5777c87d9e41d51a18ee7f3598dda9
paceto <proofofrandomness@gmail.com>
2024-10-06 20:32:20 +0300

    WIP

--- models/pipe/meta-llama/Llama-2-7b-chat-hf/default/download.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
--- models/pipe/meta-llama/Meta-Llama-3-70B/default/download.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-70B")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-70B")
